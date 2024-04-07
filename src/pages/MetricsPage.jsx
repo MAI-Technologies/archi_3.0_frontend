d4b8ff94dbac2c4114344fdbe17a09dc3cc50837
Chenhao Li <CharlesOfficial1@hotmail.com>
2024-04-07 16:50:15 -0400

    metrics done

--- src/pages/MetricsPage.jsx
+++ src/pages/MetricsPage.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react';
 import { useNavigate } from "react-router-dom"
 import styles from "./MetricsPage.module.css";
-import { metricsRequest } from '../request/metricsRequest';
+import { metricsRequest } from '../requests/metricsRequest';
 import { metricsPasswordRequest } from '../requests/metricsPasswordRequest';
 
 export default function MetricsPage() {
@@ -10,7 +10,7 @@ export default function MetricsPage() {
     const passwordRef = useRef();
     const startDateRef = useRef();
     const endDateRef = useRef();
-    const [metrics, setMetrics] = useState([]);
+    const [metrics, setMetrics] = useState({});
     const [madeRequest, setMadeRequest] = useState(false);
     const accordionDiv = useRef();
 
@@ -69,6 +69,50 @@ export default function MetricsPage() {
 
             <button>Submit</button>
         </form><br></br>
+        {JSON.stringify(metrics) !== "{}" && <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Total Number of Users</td>
+                        <td>{metrics.totalNumOfStudents}</td>
+                    </tr>
+                    <tr>
+                        <td>Number of Users that are active last week</td>
+                        <td>{metrics.activeAgainAfterOneWeek}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Number of Inputs</td>
+                        <td>{metrics.totalInputs}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Number of Outputs</td>
+                        <td>{metrics.totalOutputs}</td>
+                    </tr>
+                    <tr>
+                        <td>Percentage of Website Visitors who clicked Get Started</td>
+                        <td>{(metrics.webStats.totalNumberOfVisitorsWhoClickedOnGetStarted / metrics.webStats.totalNumberOfVisitors
+                            || 0).toLocaleString(undefined, { style: "percent" })}</td>
+                    </tr>
+                    <tr>
+                        <td>Percent of website visitors that make an account after clicking on Get Started</td>
+                        <td>{(metrics.webStats.totalNumberOfVisitorsWhoMadeAnAccount / metrics.webStats.totalNumberOfVisitorsWhoClickedOnGetStarted || 0).toLocaleString(undefined, { style: "percent" })}</td>
+                    </tr>
+                    <tr>
+                        <td>Average Time for Chatbot to respond to all users</td>
+                        <td>{metrics.webStats.totalChatbotResponseTimes / metrics.webStats.chatbotResponseTimes.length || 0} ms</td>
+                    </tr>
+                    <tr>
+                        <td>Average Clicks of the Calculator Button per User</td>
+                        <td>{metrics.webStats.totalCalculatorClicks / metrics.totalNumOfStudents || 0} times</td>
+                    </tr>
+                    <tr>
+                        <td>Percent Who Return to Chatbot After the First Interaction after 24 hours</td>
+                        <td>{(metrics.webStats.userWhoReturnToChatbotAfter24InitialConversation / metrics.totalNumOfStudents || 0).toLocaleString(undefined, { style: "percent" })}</td>
+                    </tr>
+                </tbody>
+
+            </table>
+        </div>}
         {metrics.length > 0 ? <div ref={accordionDiv} className={styles.metrics}>
             <div className={styles.totalSessions}>{metrics.length} sessions found</div>
             {metrics.map(metric => {
--- src/requests/metricsRequest.js
+++ src/requests/metricsRequest.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export async function metricsRequest(startDate, endDate) {
     try {
-        const res = await axios.post("https://archi2-backend-d33aae681e67.herokuapp.com/metrics/dates", {
+        const res = await axios.post("http://localhost:4000/metrics/dates", {
             startDate, endDate
         });
 
-        return res.data?.data;
+        return res.data;
     } catch (err) {
         throw err;
     }
