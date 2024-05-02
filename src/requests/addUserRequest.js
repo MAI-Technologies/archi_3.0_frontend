35fa47c79482fbd7b1a2e3dadf454e604e3b6edc
Chenhao Li <CharlesOfficial1@hotmail.com>
2024-05-01 20:39:31 -0400

    switched backend url

--- src/requests/addUserRequest.js
+++ src/requests/addUserRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function addUserRequest(userId, firstName, lastName, dob, email, password, signupType) {
     try {
-        const res = await axios.post("http://localhost:4000/user/add-user", {
+        const res = await axios.post("https://archi-3-backend-fabe5cbde85f.herokuapp.com/user/add-user", {
             userId,
             firstName,
             lastName,
